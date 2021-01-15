30da3d4ce82d06fb173b77602068a9cbe057a5f9
Erwan Vallée <erwanp.vallee@gmail.com>
2021-01-15 13:37:27 +0100

    update

--- src/components/utils/Loading.js
+++ src/components/utils/Loading.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default () => {
+const loader = () => {
   return (
-    <div style={ { minHeight: '100vh'} } className="d-flex flex-row justify-content-center align-items-center w-100">
+    <div
+      style={{ minHeight: '100vh' }}
+      className="d-flex flex-row justify-content-center align-items-center w-100"
+    >
       <img alt="loading gif" src="https://i.redd.it/ounq1mw5kdxy.gif" />
     </div>
   );
-}
+};
+export default loader;
